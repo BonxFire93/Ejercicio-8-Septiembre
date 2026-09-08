99a7e4316dde4abd5262530268aff5019b0dda2a
BonXFire69 <chiletub@gmail.com>
2026-09-08 11:54:52 -0300

    v0.5

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect # type: ignore
+from flask import Flask, render_template, redirect, request #type: ignore
 
 app = Flask(__name__)
 
@@ -13,6 +13,16 @@ def registro():
 @app.route('/lista')
 def lista():
     return render_template('lista.html')
+
+@app.route('/procesar_registro', methods=['POST'])
+def procesar_registro():
+    registros = {
+        'nombre': request.form['nombre'],
+        'apellido': request.form['apellido'],
+        'edad': request.form['edad']
+    }
+    registro.save(registros)
+    return redirect('/lista')
 
 if __name__ == '__main__':
     app.run(debug=True)
